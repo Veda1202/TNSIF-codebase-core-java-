73015fce9aff5afbfafe8d7f9634d5d005f6d93e
Vedanti Rakesh Rana <Vedanti Rakesh Rana@LAPTOP-A7TQAUV8>
2025-07-07 20:34:20 +0530

    DaySeven

---
+++ DaySeven/src/overriding/CovarientOverridingDemo.java
@@ -0,0 +1,45 @@
+package overriding;
+
+
+//Parent class
+class Colour{
+
+	protected Colour getColour()
+	{
+		Colour s=new Colour();
+		return s;
+	}
+}
+//child class
+class Red extends Colour{
+	
+	@Override
+	protected Red getColour()
+	{
+		Red s=new Red();
+		return s;
+	}
+	
+	
+}
+//child class
+class Blue extends Colour{
+	
+	@Override
+	protected Blue getColour()
+	{
+		Blue s=new Blue();
+		return s;
+	}
+	
+	
+}
+
+public class CovarientOverridingDemo {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
